b001c51b74406d6882e4b259918008dcd927f148
Maria <m.manishamiranda@gmail.com>
2023-12-18 14:25:53 -0700

    Transformation script

--- data-source/transformations/transformation2.py
+++ data-source/transformations/transformation2.py
@@ -41,7 +41,7 @@ def convert_data(input_data):
     return converted_data
 
 def write_data(data, output_path):
-    data.repartition(1).write().mode("overwrite").csv(output_path)
+    data.repartition(1).write.mode("overwrite").csv(output_path)
 
 if __name__ == "__main__":
     main()
