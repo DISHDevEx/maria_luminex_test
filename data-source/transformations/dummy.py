66663c4ff1719f2dc5f7dd8f2b7e30d3e6705cce
Maria <m.manishamiranda@gmail.com>
2023-12-19 13:00:10 -0700

    Transformation script

--- data-source/transformations/dummy.py
+++ data-source/transformations/dummy.py
@@ -1,35 +1,26 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import sum
 import sys
 
 
 def main():
     # Create a Spark session
     spark = SparkSession.builder.appName("Transformation1").getOrCreate()
-    print(spark)
 
     input_path, output_path = get_input_output_paths()
-    print(input_path)
-    print(output_path)
 
-    # Reading data
-    input_data = process_s3_data(spark, input_path)
+    # Read data
+    input_data = read_data(spark, input_path)
     input_data.show()
-    # input_data = read_data(spark, input_path)
-    # input_data.show()
 
-    # # Transformation
-    # transformed_data = transformation_1(input_data)
-    # transformed_data.show()
+    # Transformation
+    transformed_data = transformation_1(input_data)
+    transformed_data.show()
 
     # Write the converted data to the output path
-    # write_data(input_data, output_path)
+    write_data(transformed_data, output_path)
 
     # Stop the Spark session
     spark.stop()
-
-# def read_data(spark, input_path):
-#     return spark.read.csv(input_path, header=True)
 
 
 def get_input_output_paths():
@@ -47,108 +38,48 @@ def get_input_output_paths():
     return input_path, output_path
 
 
-# def read_data(spark, input_path):
-#     """
-#     Main function to initiate the S3 data processing.
-#     """
-#     # print("ksjhcfjkfhgj----------Read data")
-#     # print(input_path)
-#     # process_s3_data(spark, input_path)
-#     return spark.read.csv(input_path, header=True)
-
-
-def read_csv_to_df(spark, input_path):
-    """
-    Reads CSV data from an S3 bucket folder path into a Spark DataFrame.
-
-    Parameters:
-        bucket_folder_path (str): The S3 bucket folder path containing the CSV files.
-
-    Returns:
-        df: A Spark DataFrame representing the CSV data.
-    """
-    print("ckhsjchsjh- Read CSV")
-    print(input_path)
-    df = spark.read.csv(input_path, header=True)
-
-    return df
-
-def read_json_to_df(spark, input_path):
-    """
-    Reads data from a JSON file into a Spark DataFrame.
-
-    Parameters:
-        bucket_folder_path (str): The file path of the JSON file.
-
-    Returns:
-        df: A Spark DataFrame representing the JSON data.
-    """
-    df = spark.read.json(input_path, multiLine=True)
-
-    return df
-
-def read_parquet_to_df(spark, input_path):
-    """
-    Reads data from a Parquet file into a Spark DataFrame.
-
-    Parameters:
-        bucket_folder_path (str): The file path of the Parquet file.
-
-    Returns:
-        df: A Spark DataFrame representing the Parquet data.
-    """
-    df = spark.read.parquet(input_path)
-
-    return df
-
-
-def process_s3_data(spark, input_path):
-    """
-    Processes data in an S3 bucket based on the file extension.
-
-    Parameters:
-        bucket_folder_path (str): The S3 bucket folder path containing the data files.
-
-    Returns:
-        None
-    """
-    df = spark.read.csv(input_path, header=True)
-    # df = None
-    # df = read_csv_to_df(spark, input_path)
-
+def read_data(spark, input_path):
     # Choose the appropriate method based on the file extension
-    # if bucket_folder_path.lower().endswith(".json"):
-    #     df = read_json_to_df(spark, bucket_folder_path)
-    # if bucket_folder_path.lower().endswith(".csv"):
-    #     print(bucket_folder_path)
-    #     df = read_csv_to_df(spark, bucket_folder_path)
-    # if bucket_folder_path.lower().endswith(".parquet"):
-    #     df = read_parquet_to_df(spark, bucket_folder_path)
-
+    if input_path.lower().endswith(".json"):
+        df = spark.read.json(input_path, multiLine=True)
+    if input_path.lower().endswith(".csv"):
+        df = spark.read.csv(input_path, header=True)
+    if input_path.lower().endswith(".parquet"):
+        df = spark.read.parquet(input_path)
     return df
 
 
-# def transformation_1(input_df):
-#     """
-#     Applies a transformation to calculate total sales for each product in a PySpark DataFrame.
-#
-#     Parameters:
-#     -----------
-#     input_df : pyspark.sql.DataFrame
-#         Input PySpark DataFrame with columns: 'Product', 'Revenue', and other optional columns.
-#
-#     Returns:
-#     --------
-#     pyspark.sql.DataFrame
-#         Transformed DataFrame with columns:
-#             - 'Product': Unique products.
-#             - 'TotalSales': Total sales for each product.
-#
-#     """
-#     output_df = input_df.groupBy('Product').agg(sum('Revenue').alias('TotalSales'))
-#     return output_df
+def convert_data(input_data):
+    converted_data = input_data.drop('Weight')
+    # input_data.withColumnRenamed("Revenue", "Price")
+    # input_data.withColumn('Total', col('Revenue') * col('Quantity'))
+    return converted_data
+
+
+def transformation_1(input_data):
+    """
+    Applies a transformation to calculate total sales for each product in a PySpark DataFrame.
+
+    Parameters:
+    -----------
+    input_df : pyspark.sql.DataFrame
+        Input PySpark DataFrame with columns: 'Product', 'Revenue', and other optional columns.
+
+    Returns:
+    --------
+    pyspark.sql.DataFrame
+        Transformed DataFrame with columns:
+            - 'Product': Unique products.
+            - 'TotalSales': Total sales for each product.
+
+    """
+    output_df = input_data.drop('Weight')
+    return output_df
 
 
 def write_data(data, output_path):
-    data.repartition(1).write.mode("overwrite").option("header", "true").json(output_path)
+    data.repartition(1).write.mode("overwrite").option("header", "true").csv(output_path)
 
+
+if __name__ == "__main__":
+    main()
--- data-source/transformations/transformation1.py
+++ data-source/transformations/transformation1.py
@@ -1,29 +1,26 @@
 from pyspark.sql import SparkSession
-import sys
-
+from pyspark.sql.functions import col
 
 def main():
     # Create a Spark session
-    spark = SparkSession.builder.appName("Transformation1").getOrCreate()
+    spark = SparkSession.builder.appName("Transformation3").getOrCreate()
 
     input_path, output_path = get_input_output_paths()
 
     # Read data
     input_data = read_data(spark, input_path)
     input_data.show()
-
-    # Transformation
-    transformed_data = transformation_1(input_data)
-    transformed_data.show()
+    converted_data = convert_data(input_data)
+    converted_data.show()
 
     # Write the converted data to the output path
-    write_data(transformed_data, output_path)
+    write_data(converted_data, output_path)
 
     # Stop the Spark session
     spark.stop()
 
-
 def get_input_output_paths():
+    import sys
 
     if len(sys.argv) != 5:
         print("Usage: spark-submit script.py --input <input_path> --output <output_path>")
@@ -37,49 +34,13 @@ def get_input_output_paths():
 
     return input_path, output_path
 
-
 def read_data(spark, input_path):
-    # Choose the appropriate method based on the file extension
-    if input_path.lower().endswith(".json"):
-        df = spark.read.json(input_path, multiLine=True)
-    if input_path.lower().endswith(".csv"):
-        df = spark.read.csv(input_path, header=True)
-    if input_path.lower().endswith(".parquet"):
-        df = spark.read.parquet(input_path)
-    return df
-
+    return spark.read.csv(input_path, header=True)
 
 def convert_data(input_data):
-    converted_data = input_data.drop('Weight')
-    # input_data.withColumnRenamed("Revenue", "Price")
-    # input_data.withColumn('Total', col('Revenue') * col('Quantity'))
+    converted_data = input_data.withColumn("Total", col("Price") * col("Quantity"))
     return converted_data
 
-
-def transformation_1(input_data):
-    """
-    Applies a transformation to calculate total sales for each product in a PySpark DataFrame.
-
-    Parameters:
-    -----------
-    input_df : pyspark.sql.DataFrame
-        Input PySpark DataFrame with columns: 'Product', 'Revenue', and other optional columns.
-
-    Returns:
-    --------
-    pyspark.sql.DataFrame
-        Transformed DataFrame with columns:
-            - 'Product': Unique products.
-            - 'TotalSales': Total sales for each product.
-
-    """
-    output_df = input_data.drop('Weight')
-    return output_df
-
-
 def write_data(data, output_path):
+    # data.repartition(1).write().mode("overwrite").csv(output_path)
     data.repartition(1).write.mode("overwrite").option("header", "true").csv(output_path)
-
-
-if __name__ == "__main__":
-    main()
