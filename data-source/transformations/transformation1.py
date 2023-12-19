9a285d875b50dc918f8f19c6a23719f2863be152
Maria <m.manishamiranda@gmail.com>
2023-12-19 15:07:37 -0700

    Transformation script

--- data-source/transformations/transformation1.py
+++ data-source/transformations/transformation1.py
@@ -12,11 +12,11 @@ def main():
     input_data = read_data(spark, input_path)
     input_data.show()
     spark.sparkContext.setLogLevel("ERROR")
-    converted_data = convert_data(input_data)
-    converted_data.show()
+    # converted_data = convert_data(input_data)
+    # converted_data.show()
 
     # Write the converted data to the output path
-    write_data(converted_data, output_path)
+    write_data(input_data, output_path)
 
     # Stop the Spark session
     spark.stop()
@@ -39,12 +39,12 @@ def get_input_output_paths():
 def read_data(spark, input_path):
     return spark.read.csv(input_path, header=True)
 
-def convert_data(spark, input_data):
-    # converted_data = input_data.withColumn("Weight", col("Weight")*100)
-    # # df.withColumn("salary",col("salary")*100)
-    input_data.createOrReplaceTempView("input_data")
-    converted_data = spark.sql("SELECT *, (Weight * 2) AS Total FROM input_data").show()
-    return converted_data
+# def convert_data(spark, input_data):
+#     # converted_data = input_data.withColumn("Weight", col("Weight")*100)
+#     # # df.withColumn("salary",col("salary")*100)
+#     input_data.createOrReplaceTempView("input_data")
+#     converted_data = spark.sql("SELECT *, (Weight * 2) AS Total FROM input_data").show()
+#     return converted_data
 
 def write_data(data, output_path):
     # data.repartition(1).write().mode("overwrite").csv(output_path)
