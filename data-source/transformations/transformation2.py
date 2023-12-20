70df03403c0a2994a8b39c34ea44ba9034eb0b16
Maria <m.manishamiranda@gmail.com>
2023-12-20 11:33:00 -0700

    Transformation script

--- data-source/transformations/transformation2.py
+++ data-source/transformations/transformation2.py
@@ -4,10 +4,17 @@ import sys
 
 
 def main():
+    """
+        Main function which that runs a spark job to convert a dataset into dataframe,
+        apply the transformation and next convert the transformed dataframe into json and store to an S3.
+
+    """
     # Create a Spark session
     spark = SparkSession.builder.appName("Transformation2").getOrCreate()
 
     input_path, output_path = get_input_output_paths()
+
+    output_format = "json"
 
     # Read data
     input_data = read_data(spark, input_path)
@@ -18,14 +25,20 @@ def main():
     transformed_data.show()
 
     # Write the converted data to the output path
-    write_data(transformed_data, output_path)
+    pyspark_df_json_upload(transformed_data, output_format, output_path)
 
     # Stop the Spark session
     spark.stop()
 
 
 def get_input_output_paths():
+    """
+        Retrieves the S3 path of the input dataset and the S3 path to save the transformed dataset.
 
+        Returns:
+        - input_path: S3 path of the input dataset
+        - output_path: S3 path to save the transformed dataset
+    """
     if len(sys.argv) != 5:
         print("Usage: spark-submit script.py --input <input_path> --output <output_path>")
         sys.exit(1)
@@ -40,6 +53,15 @@ def get_input_output_paths():
 
 
 def read_data(spark, input_path):
+    """
+        Reads the input file from an S3 bucket and returns a DataFrame.
+
+        Parameters:
+        - input_path: S3 path of the input dataset
+
+        Returns:
+        - dataframe: Dataframe containing the input data.
+    """
     # Choose the appropriate method based on the file extension
     if input_path.lower().endswith(".json"):
         df = spark.read.json(input_path, multiLine=True)
@@ -66,7 +88,6 @@ def transformation_2(input_data):
         Transformed DataFrame with additional columns:
             - 'SalesPerQuantity': Calculated as 'Revenue' divided by 'Quantity'.
             - 'SalesPerWeight': Calculated as 'Revenue' divided by 'Weight'.
-            - 'Date': Converted to timestamp and formatted as 'dd-LLL-yyyy'.
 
     """
     input_data = input_data.withColumn("SalesPerQuantity", f.col("Revenue") / f.col("Quantity"))
@@ -75,8 +96,15 @@ def transformation_2(input_data):
     return transformed_data
 
 
-def write_data(data, output_path):
-    data.repartition(1).write.mode("overwrite").option("header", "true").csv(output_path)
+def pyspark_df_json_upload(df, output_format, output_path):
+
+    """
+    Converts the transformed spark dataframe to desired format and saves the output to s3 bucket.
+    df : pyspark dataframe
+    output_format : format of the transformed-data
+    output_path : output data stored location in s3
+    """
+    df.repartition(1).write.format(output_format).mode("overwrite").save(output_path)
 
 
 if __name__ == "__main__":
--- data-source/transformations/transformation3.py
+++ data-source/transformations/transformation3.py
@@ -5,6 +5,11 @@ from pyspark.sql.functions import col, cast, sum, year, month
 
 
 def main():
+    """
+        Main function which that runs a spark job to convert a dataset into dataframe,
+        apply the transformation and next convert the transformed dataframe into json and store to an S3.
+
+    """
     # Create a Spark session
     spark = SparkSession.builder.appName("Transformation3").getOrCreate()
 
@@ -18,15 +23,23 @@ def main():
     transformed_data = transformation_3(input_data)
     transformed_data.show()
 
+    output_format = "json"
+
     # Write the converted data to the output path
-    write_data(transformed_data, output_path)
+    pyspark_df_json_upload(transformed_data, output_format, output_path)
 
     # Stop the Spark session
     spark.stop()
 
 
 def get_input_output_paths():
+    """
+        Retrieves the S3 path of the input dataset and the S3 path to save the transformed dataset.
 
+        Returns:
+        - input_path: S3 path of the input dataset
+        - output_path: S3 path to save the transformed dataset
+    """
     if len(sys.argv) != 5:
         print("Usage: spark-submit script.py --input <input_path> --output <output_path>")
         sys.exit(1)
@@ -41,6 +54,15 @@ def get_input_output_paths():
 
 
 def read_data(spark, input_path):
+    """
+        Reads the input file from an S3 bucket and returns a DataFrame.
+
+        Parameters:
+        - input_path: S3 path of the input dataset
+
+        Returns:
+        - dataframe: Dataframe containing the input data.
+    """
     # Choose the appropriate method based on the file extension
     if input_path.lower().endswith(".json"):
         df = spark.read.json(input_path, multiLine=True)
@@ -65,6 +87,7 @@ def transformation_3(input_data):
     --------
     pyspark.sql.DataFrame
         Transformed DataFrame with additional columns:
+            - 'Date': Converted to timestamp and formatted as 'dd-LLL-yyyy'.
             - 'Year': Extracted from the 'Date' column.
             - 'Month': Extracted from the 'Date' column.
             - 'TotalSales_Product': Total sales for each product.
@@ -97,9 +120,15 @@ def transformation_3(input_data):
     return transformed_data
 
 
-def write_data(data, output_path):
-    data.repartition(1).write().mode("overwrite").csv(output_path)
-    # data.write.mode("overwrite").csv(output_path)
+def pyspark_df_json_upload(df, output_format, output_path):
+
+    """
+    Converts the transformed spark dataframe to desired format and saves the output to s3 bucket.
+    df : pyspark dataframe
+    output_format : format of the transformed-data
+    output_path : output data stored location in s3
+    """
+    df.repartition(1).write.format(output_format).mode("overwrite").save(output_path)
 
 
 if __name__ == "__main__":
