1df865ac536e7cbcbd7b4afe2883fd2809f2d5a9
Maria <m.manishamiranda@gmail.com>
2023-12-20 16:33:51 -0700

    Transformation script

--- data-source/transformations/transformation4.py
+++ data-source/transformations/transformation4.py
@@ -156,15 +156,15 @@ def pyspark_df_json_upload(df, output_format, output_path):
     output_format : format of the transformed-data
     output_path : output data stored location in s3
     """
-    # filename = "transformation.json"
-    # # df.repartition(1).write.format(output_format).mode("overwrite").option("header", "true").save(output_path)
-    # df.repartition(1).write.mode("overwrite").option("header", "true").json(output_path + '/' + filename)
     filename = "transformation.json"
-    full_s3_path = output_path + '/' + filename
-    df.repartition(1).write \
-        .mode("overwrite") \
-        .option("header", "true") \
-        .json(full_s3_path)
+    # df.repartition(1).write.format(output_format).mode("overwrite").option("header", "true").save(output_path)
+    df.coalesce(1).write.mode("overwrite").option("header", "true").json(output_path + '/' + filename)
+    # filename = "transformation.json"
+    # full_s3_path = output_path + '/' + filename
+    # df.repartition(1).write \
+    #     .mode("overwrite") \
+    #     .option("header", "true") \
+    #     .json(full_s3_path)
 
     # df.repartition(1).write.mode("overwrite").option("header", "true").save(full_s3_path, format='json')
 
